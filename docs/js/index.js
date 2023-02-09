5b4ad49ba8275ef92ec53908d2d00eb04c51a93d
EkaterinaKochneva <kochnevaes@gmail.com>
2023-02-09 18:06:14 +0300

    third

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    // Калькулятор расчета стоимости
+    $('.calculator__button-next-step').click(function (e) {	
+        e.preventDefault();
+        $(this).parents(".calculator__step").removeClass("active");
+        $(this).parents(".calculator__step").next().addClass("active");	
+        const show_id = $(this).attr('href').substring(1);
+        $('.calculator__counter-item').removeClass('active');
+        $('.calculator__counter-item[data-step="'+show_id+'"]').addClass('active');
 
+	});
 
 })
