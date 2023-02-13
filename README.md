f50a689bc67c62fe647bb990698cef7982740b74
EkaterinaKochneva <kochnevaes@gmail.com>
2023-02-13 18:07:11 +0300

    finish category

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1. https://ekaterinakochneva.github.io/dom-navek/
 2. http://localhost:3000/catalog.html
+3. http://localhost:3000/category.html
--- docs/js/page.js
+++ docs/js/page.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+    $('.collaps-btn-js').on('click', function () {
+        $(this).toggleClass('collapsed');
+        if($(this).hasClass('collapsed')) {
+            $(this).text($(this).data('collapsed-disabled'));
+        } else {
+            $(this).text($(this).data('collapsed-enabled'));
+        }
+        $(this).next().fadeToggle();
+    });
+    
+    
+    const stockSlider = new Swiper('.stock-slider__swiper', {
+		slidesPerView: 1,
+        spaceBetween: 15,
+        loop: true,
+		autoplay: {
+			delay: 8000,
+		},
+        pagination: {
+            el: '.stock-slider__pagination',
+            clickable: true,
+          },
+
+	});
 
 })
